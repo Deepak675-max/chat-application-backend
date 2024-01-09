333f9acba9a3a1bfc73b93ef66558e36ebf2d7e7
Deepak675-max <deepakkamboj.kamboj.675@gmail.com>
2024-01-09 15:07:50 +0530

    updated cors origin

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const initializeSocket = require('./helper/common/init_socket.io');
 initializeSocket(server);
 
 chatAppBackend.use(cors({
-    origin: "http://127.0.0.1:5500"
+    origin: "http://13.200.172.204"
 }));
 
 const sequelize = require('./helper/common/init_mysql');
